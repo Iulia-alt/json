2e44bc0ae59e87c8b73cb3327805de28c59379a6
Iulia <l7n8@mail.ru>
2024-01-03 22:09:12 +0200

    up

--- index.js
+++ index.js
@@ -8,20 +8,19 @@ data.forEach(({ image, name, info }) => {
  const cardEl = document.createElement('div');
  cardEl.classList.add('card');
  divEl.append(cardEl);
- divEl.style.textAlign = 'center';
  
  const img = document.createElement('img');
  img.src = image;
  img.width = 250;
- divEl.append(img); 
+ cardEl.append(img); 
 
  const nameEl = document.createElement('h4');
  nameEl.classList.add('name');
  nameEl.textContent = `${name}`;
- divEl.append(nameEl);
+ cardEl.append(nameEl);
 
  const infoEl = document.createElement('p');
  infoEl.classList.add('info');
  infoEl.textContent = `${info}`;
- divEl.append(infoEl);
+ cardEl.append(infoEl);
 });
